4f87a16316f0d18474b756d6e1ab9d6b93f768d1
Melih Akyol <melih@rettermobile.com>
2021-04-10 23:27:09 +0300

    changed port option

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ app.get('/', (req, res) => {
     res.send('Hello there')
 })
 
-app.listen(3000, () => {
+const port = process.env.port || 3000
+app.listen(port, () => {
     console.log('started')
 })
